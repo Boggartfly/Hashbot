764101b470c4c3a434d2d2ac5b198dfb97c58983
Parth Sane <laerdevstudios@gmail.com>
2014-01-31 19:51:33 +0530

    Minor Commit

---
+++ src/code/laerstudios/hashbot/FileChooser.java
@@ -0,0 +1,103 @@
+package code.laerstudios.hashbot;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+import code.laerstudios.hashbot.R;
+
+public class FileChooser extends ListActivity {
+
+	public FileChooser() {
+		// TODO Auto-generated constructor stub
+	}
+	private File currentDir;
+	private FileArrayAdapter adapter;
+	static String EXTRA_MESSAGE="code.laerstudios.FileChooser";
+	
+	
+	 @Override
+	
+	     public void onCreate(Bundle savedInstanceState) {
+	 
+	         super.onCreate(savedInstanceState);
+	         currentDir = new File( Environment.getExternalStorageDirectory().getPath());
+	         fill(currentDir);
+	       
+	 
+	     }
+	
+	 private void fill(File f)
+	    {
+	        File[]dirs = f.listFiles();
+	         this.setTitle("Current Dir: "+f.getName());
+	         List<Option>dir = new ArrayList<Option>();
+	         List<Option>fls = new ArrayList<Option>();
+	         try{
+	             for(File ff: dirs)
+	             {
+	                if(ff.isDirectory())
+	                    dir.add(new Option(ff.getName(),"Folder",ff.getAbsolutePath()));
+	                else
+	                {
+	                    fls.add(new Option(ff.getName(),"File Size: "+ff.length(),ff.getAbsolutePath()));
+	                }
+	             }
+	         }catch(Exception e)
+	         {
+	             
+	         }
+	         Collections.sort(dir);
+	         Collections.sort(fls);
+	         dir.addAll(fls);
+	         if(!f.getName().equalsIgnoreCase("sdcard"))
+	             dir.add(0,new Option("..","Parent Directory",f.getParent()));
+	         
+	         adapter = new FileArrayAdapter(FileChooser.this,R.layout.file_view,dir);
+	         
+	                  this.setListAdapter(adapter);
+
+	    }
+
+	 @Override
+		protected void onListItemClick(ListView l, View v, int position, long id) {
+			// TODO Auto-generated method stub
+			super.onListItemClick(l, v, position, id);
+			Option o = adapter.getItem(position);
+			if(o.getData().equalsIgnoreCase("folder")||o.getData().equalsIgnoreCase("parent directory")){
+					currentDir = new File(o.getPath());
+					fill(currentDir);
+			}
+			else
+			{
+				onFileClick(o);
+			}
+
+
+		}
+	 private void onFileClick(Option o)
+	 
+	     {
+		     Intent returnintent = new Intent(this, MainActivity.class);
+		     
+	         Toast.makeText(this, "File Selected: "+o.getName(), Toast.LENGTH_SHORT).show();
+	        String path=o.getPath();
+	        String name=o.getName();
+	        returnintent.putExtra("filepath", path);
+	        returnintent.putExtra("filename", name);
+	        setResult(RESULT_OK,returnintent);
+	        
+	        finish();
+	     }
+
+
+
+}
---
+++ src/code/laerstudios/hashbot/MainActivity.java
@@ -0,0 +1,155 @@
+package code.laerstudios.hashbot;
+
+
+
+
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import code.laerstudios.hashbot.R;
+public class MainActivity extends Activity {
+	 private AdView adView;
+	 private static String MY_AD_UNIT_ID="a152e534110e222";
+	 private static final String TAG = "Adbuild";
+	
+	 @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+       
+		
+		
+
+	                
+		adView = new AdView(this);
+	    adView.setAdUnitId(MY_AD_UNIT_ID);
+	    adView.setAdSize(AdSize.BANNER);
+	    RelativeLayout layout = (RelativeLayout)findViewById(R.id.mainLayout);
+	    layout.addView(adView);
+	    AdRequest adRequest = new AdRequest.Builder().addTestDevice("E05D4CDEB622522AFBFB3FC12EA4677C").build();
+	    Log.v(TAG, "(ads) isTestDevice=" + adRequest.isTestDevice(this));
+	    adView.loadAd(adRequest);
+	 }
+	
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	    case R.id.upgrade:
+        {	
+        	
+
+        	Uri uri = Uri.parse("market://details?id=code.laerstudios.hashbotpro");
+            Intent myAppLinkToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            try {
+                startActivity(myAppLinkToMarket);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(this, " unable to find market app", Toast.LENGTH_LONG).show();
+            }
+	      
+
+	       
+       return true;
+        }    
+	    
+	    case R.id.opengit:
+	            {
+	            	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://boggartfly.github.io/Hashbot"));
+	            	startActivity(browserIntent);
+	            	return true;
+	            }
+	        
+	    case R.id.rate:
+	        { 
+	        	Uri uri = Uri.parse("market://details?id=" + getPackageName());
+	            Intent myAppLinkToMarket = new Intent(Intent.ACTION_VIEW, uri);
+	            try {
+	                startActivity(myAppLinkToMarket);
+	            } catch (ActivityNotFoundException e) {
+	                Toast.makeText(this, " unable to find market app", Toast.LENGTH_LONG).show();
+	            }
+	        return true;
+	        }
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, FileChooser.class);
+		startActivityForResult(intent,1);
+	}
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		  if (requestCode == 1) {
+
+		     if(resultCode == RESULT_OK){      
+		         String resultantpath=data.getStringExtra("filepath");
+		         String name=data.getStringExtra("filename");
+		         if(resultantpath!=null)
+		         {	 
+		        	 Intent intent = new Intent(this, DigestUtil.class);
+		         	 
+		         	intent.putExtra("path", resultantpath);
+		         	intent.putExtra("filename", name);
+		         	 startActivityForResult(intent,2);
+		         	 
+		         }
+		     }
+		     if (resultCode == RESULT_CANCELED) {    
+		         //Write your code if there's no result
+		     }
+		  }
+		//onActivityResult
+	 if (requestCode == 2) {
+
+		 if(resultCode == RESULT_OK){      
+	         String sha1=data.getStringExtra("sha1");
+	         String sha256=data.getStringExtra("sha256");
+	         String sha512=data.getStringExtra("sha512");
+	         String md5=data.getStringExtra("md5");
+	          
+	         TextView hashtext=(TextView) findViewById(R.id.text_id);
+	        if(hashtext!=null)
+	         {
+hashtext.setText("SHA-1 hash:-\n"+sha1+"\n\n"+"SHA-256 hash:-\n"+sha256+"\n\n"+"SHA-512 hash:-\n"+sha512+"\n\n"+"MD5 hash:-\n"+md5);
+	    
+	         }
+	        else
+	        	Toast.makeText(this, "hashtext is null", Toast.LENGTH_SHORT).show();	
+		 }
+	     if (resultCode == RESULT_CANCELED) {    
+	         //Write your code if there's no result
+	     }
+	  }
+	//onActivityResult
+	}
+
+	   
+
+}
