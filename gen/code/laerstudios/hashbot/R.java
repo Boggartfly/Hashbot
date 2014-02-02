06b43e6fb99702d6c4d2395229fc561d6460aac6
Parth Sane <laerdevstudios@gmail.com>
2014-02-02 16:52:43 +0530

    Updated UI

--- gen/code/laerstudios/hashbot/R.java
+++ gen/code/laerstudios/hashbot/R.java
@@ -226,26 +226,31 @@ containing a value of this type.
         public static final int wait=0x7f020020;
     }
     public static final class id {
-        public static final int TextView01=0x7f050008;
-        public static final int TextView02=0x7f050009;
+        public static final int TextView01=0x7f05000b;
+        public static final int TextView02=0x7f05000c;
         public static final int button1=0x7f050006;
+        public static final int dialogButtonOK=0x7f05000a;
+        public static final int help=0x7f050010;
         public static final int hybrid=0x7f050004;
+        public static final int image=0x7f050008;
         public static final int mainLayout=0x7f050005;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
-        public static final int opengit=0x7f05000b;
-        public static final int rate=0x7f05000c;
+        public static final int opengit=0x7f05000e;
+        public static final int rate=0x7f05000f;
         public static final int satellite=0x7f050002;
         public static final int terrain=0x7f050003;
+        public static final int text=0x7f050009;
         public static final int text_id=0x7f050007;
-        public static final int upgrade=0x7f05000a;
+        public static final int upgrade=0x7f05000d;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int file_view=0x7f030001;
+        public static final int dialog=0x7f030001;
+        public static final int file_view=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
@@ -350,7 +355,9 @@ containing a value of this type.
         public static final int dlg_choosedir_title=0x7f06001c;
         public static final int git=0x7f060020;
         public static final int help=0x7f060022;
+        public static final int helpmessage=0x7f060025;
         public static final int instruction=0x7f06001f;
+        public static final int ok=0x7f060024;
         public static final int ophashes=0x7f06001e;
         public static final int rate=0x7f060021;
         public static final int select_file=0x7f06001d;
--- src/code/laerstudios/hashbot/MainActivity.java
+++ src/code/laerstudios/hashbot/MainActivity.java
@@ -10,12 +10,17 @@ import com.google.android.gms.ads.*;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +30,30 @@ public class MainActivity extends Activity {
 	 private static final String MY_AD_UNIT_ID="";
 	 private static final String TAG = "Adbuild";
 	 private InterstitialAd interstitial;
+	 final Context context = this;
+	 SharedPreferences prefs = null;
+	 String name;
 	 @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-       
+		 prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+		 if (prefs.getBoolean("firstrun", true)) {
+	            // Do first run stuff here then set 'firstrun' as false
+	            // using the following line to edit/commit prefs
+			 
+			 AlertDialog.Builder builder = new AlertDialog.Builder(context);
+	        	builder.setMessage(R.string.helpmessage)
+				       .setCancelable(false)
+				       .setIcon(R.drawable.ic_launcher)
+				       .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+				           public void onClick(DialogInterface dialog, int id) {
+				                dialog.cancel();
+				           }
+				       }).show();
+	            prefs.edit().putBoolean("firstrun", false).commit();
+	        }
+	    
 		interstitial = new InterstitialAd(this);
 	    interstitial.setAdUnitId(MY_AD_UNIT_ID);
 	    
@@ -51,7 +74,14 @@ public class MainActivity extends Activity {
 	    interstitial.setAdListener(new AdListener() {
 	        @Override
 	        public void onAdLoaded() {
-	        	interstitial.show();
+	        	prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+	        	
+	        	if (prefs.getBoolean("firstrun", false)==false) {
+		            // Do first run stuff here then set 'firstrun' as false
+		            // using the following line to edit/commit prefs
+	        		interstitial.show();
+				        }
+	        	
 	        }
 
 	        @Override
@@ -112,6 +142,19 @@ public class MainActivity extends Activity {
 	            }
 	        return true;
 	        }
+	    case R.id.help:
+        {
+        	AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        	builder.setMessage(R.string.helpmessage)
+			       .setCancelable(false)
+			       .setIcon(R.drawable.ic_launcher)
+			       .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			                dialog.cancel();
+			           }
+			       }).show();
+        	return true;
+        }
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
@@ -119,6 +162,7 @@ public class MainActivity extends Activity {
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
 	    // Do something in response to button
+		
 		Intent intent = new Intent(this, FileChooser.class);
 		startActivityForResult(intent,1);
 	}
@@ -128,7 +172,7 @@ public class MainActivity extends Activity {
 
 		     if(resultCode == RESULT_OK){      
 		         String resultantpath=data.getStringExtra("filepath");
-		         String name=data.getStringExtra("filename");
+		         name=data.getStringExtra("filename");
 		         if(resultantpath!=null)
 		         {	 
 		        	 Intent intent = new Intent(this, DigestUtil.class);
@@ -155,8 +199,9 @@ public class MainActivity extends Activity {
 	         TextView hashtext=(TextView) findViewById(R.id.text_id);
 	        if(hashtext!=null)
 	         {
-hashtext.setText("SHA-1 hash:-\n"+sha1+"\n\n"+"SHA-256 hash:-\n"+sha256+"\n\n"+"SHA-512 hash:-\n"+sha512+"\n\n"+"MD5 hash:-\n"+md5);
-	    
+	        	hashtext.setText("File Selected is: "+name+"\nSHA-1 hash:-\n"+sha1+"\n\n"+"SHA-256 hash:-\n"+sha256+"\n\n"+"SHA-512 hash:-\n"+sha512+"\n\n"+"MD5 hash:-\n"+md5);
+	        	Button p1_button = (Button)findViewById(R.id.button1);
+				p1_button.setText("Select Another File");	    
 	         }
 	        else
 	        	Toast.makeText(this, "hashtext is null", Toast.LENGTH_SHORT).show();	
